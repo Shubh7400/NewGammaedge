3ab2adce8be0dec53d8043bb0d5fd2d499cf1fff
MishraKavita <kavita.mishra@cubexo.io>
2022-11-21 17:52:17 +0530

    new changes

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ $('#sidebar').on('show.bs.collapse', function () {
 });
 
 
+$('.filters ul li').click(function(){
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: data
+    })
+  });
+  
+  var $grid = $(".grid").isotope({
+    // itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+    //   columnWidth: ".all"
+    }
+  })
